c4f8aa98d0f8f5b74beced8fa5f11b06b146c5fa
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-21 09:44:16 +0300

    Demonstrate equals, copyandtoString for Task

--- src/Task.kt
+++ src/Task.kt
@@ -1,3 +1,5 @@
+import kotlin.toString
+
 enum class Pri {
     LOW,
     MEDIUM,
@@ -7,4 +9,30 @@ enum class Pri {
 data class Task( val title: String,
                  val description: String,
                  val priority: Pri,
-                 val isCompleted: Boolean = false  )
+                 val isCompleted: Boolean = false  ){
+    override fun toString(): String {
+        return  "Ваше названия $title  Описания $description Приоритет задачи $priority Метод string()"
+    }
+
+//    override fun equals(other: Any?): String {
+//       if ( title == "Лабораторная работа 9-10 по котлину"){return title}
+//
+//    }
+
+    override fun hashCode(): Int {
+        var result = isCompleted.hashCode()
+        return result
+    }
+
+
+}
+
+fun main(){
+    val task1 = Task("Лабораторная работа 9-10 по котлину", "Мы будеи изучать много интересного ", Pri.LOW)
+    val task2 = Task("Лабораторная работа 9-10 по котлину", "Мы будеи изучать много интересного ", Pri.LOW)
+    val task3 = Task("Лабораторная работа 9-10 по котлину", "Мы будеи изучать много интересного ", Pri.HIGH)
+
+    println(task3.toString())
+    println(task3.hashCode())
+
+}
