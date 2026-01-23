bad025fe3c5a8f60b6e6b572040e4fb5eeefc8fa
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-23 13:22:36 +0300

    Demonstrate polymorphismwithReportGenerator list

--- src/ReportGenerator.kt
+++ src/ReportGenerator.kt
@@ -1,2 +1,28 @@
-interface ReportGenerator {
-}
+//interface ReportGenerator{
+//    abstract fun generateReport(): String
+//}
+//
+//class Employee(val name: String, val position : String){
+//    fun generateReport(): String {
+//        return """
+//            Информация о сотруднике:
+//            ФИО: $name
+//            Позиция: $position
+//        """
+//    }
+//}
+//
+//class  DevelopmentDepartment(val otdel: String, val drem: String) : ReportGenerator{
+//    override fun generateReport(): String {
+//        return "Информация о отделе: $otdel\nЦели: $drem"
+//    }
+//}
+//
+//fun main(){
+//    val r: List<Any> = listOf( Employee("Иванов Иван Иванович", "Разработчик Kotlin"), DevelopmentDepartment("Разработка", "Писать чистый код") )
+//
+//    for (i in r) {
+//        println(i.generateReport)
+//        println("-----")
+//    }
+//}
