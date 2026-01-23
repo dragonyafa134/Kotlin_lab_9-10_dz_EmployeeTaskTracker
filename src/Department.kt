7eb57e6273846a14d7f7092dd1fa47d7e942ca6a
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-23 13:12:38 +0300

    ImplementDevelopmentDepartment and TestingDepartment

--- src/Department.kt
+++ src/Department.kt
@@ -4,3 +4,24 @@ abstract class Department (
     abstract fun printDepartmentGoal()
 
 }
+
+class DevelopmentDepartment : Department("Разрабокта"){
+    override fun printDepartmentGoal() {
+        println("Писать чистый код")
+    }
+}
+
+
+class TestingDepartment : Department("Тестирование"){
+    override fun printDepartmentGoal() {
+        println("Находить все баги")
+    }
+}
+
+fun main(){
+    val d = DevelopmentDepartment()
+    val t = TestingDepartment()
+
+    d.printDepartmentGoal()
+    t.printDepartmentGoal()
+}
